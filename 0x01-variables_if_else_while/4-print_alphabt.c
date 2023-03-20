1720c5c22cf7535329a0d525a7eab0cb11d690f2
Aderynlola <aderinlola01@gmail.com>
2023-03-20 21:26:41 +0000

    escaping q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase excluding q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
