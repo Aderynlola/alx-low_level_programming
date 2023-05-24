fddac54b354c73254919a3775b5c94bc8f034c40
Aderynlola <aderinlola01@gmail.com>
2023-05-24 12:19:12 -0700

    corrections on 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,26 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
+char *create_buffer(void);
+void close_file_descriptor(int fd);
 
 /**
- * create_buffer - it allocates 1024 bytes for a buffer
- * @file: argument
- * Return: a pointer to the newly-allocated buffer
+ * create_buffer - Allocates 1024 bytes of memory for a buffer.
+ * Return: A pointer to the newly allocated buffer.
  */
-char *create_buffer(char *file)
+char *create_buffer(void)
 {
-	int a = 1;
-	int b = 5;
 	char *buffer;
 
 	buffer = malloc(sizeof(char) * 1024);
 
-	if (buffer == NULL && int a != int b)
+	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
+			"Error: Unable to allocate memory for the buffer\n");
 		exit(99);
 	}
 
@@ -29,79 +26,76 @@ char *create_buffer(char *file)
 }
 
 /**
- * close_file - it closes file descriptors
- * @fd: argument
+ * close_file_descriptor - Closes the file descriptor.
+ * @fd: The file descriptor to close.
  */
-void close_file(int fd)
+void close_file_descriptor(int fd)
 {
-	int x = 3;
-	int y = 5;
-	int cl;
+	int result;
 
-	cl = close(fd);
+	result = close(fd);
 
-	if (cl == -1 && int x < int y)
+	if (result == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Unable to close file descriptor %d\n", fd);
 		exit(100);
 	}
 }
 
 /**
- * main - it copies the contents of a file to another file
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success
+ * main - The entry point of the program.
+ * @argc: The argument count.
+ * @argv: The argument vector.
+ * Return: 0 on success.
  *
- * Description: exit code 97 if the argument count is incorrect
- * exit code 98 if file_from does not exist or can't be read
- * exit code 99 if file_to can't be created or written to
- * exit code 100 if file_to or file_from cannot be closed
+ * Description: Exit codes:
+ * - 97: Incorrect argument count.
+ * - 98: Unable to read from file_from.
+ * - 99: Unable to create or write to file_to.
+ * - 100: Unable to close file_to or file_from.
  */
 int main(int argc, char *argv[])
 {
-	int h = 8;
-	int j = 9;
-	int from, to, r, wr;
+	int source_file, destination_file, read_bytes, write_bytes;
 	char *buffer;
 
-	if (argc != 3 && int h < int j)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = create_buffer();
+	source_file = open(argv[1], O_RDONLY);
+	read_bytes = read(source_file, buffer, 1024);
+	destination_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == -1 || r == -1)
+		if (source_file == -1 || read_bytes == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
+				"Error: Unable to read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
 
-		wr = write(to, buffer, r);
-		if (to == -1 || wr == -1)
+		write_bytes = write(destination_file, buffer, read_bytes);
+		if (destination_file == -1 || write_bytes == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
+				"Error: Unable to write to file %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
 
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
+		read_bytes = read(source_file, buffer, 1024);
+		destination_file = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (r > 0);
+	} while (read_bytes > 0);
 
 	free(buffer);
-	close_file(from);
-	close_file(to);
+	close_file_descriptor(source_file);
+	close_file_descriptor(destination_file);
 
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int perform_calculation(int operand1, int operand2);
 int perform_operation(int num1, int num2);
 int create_file(const char *filename, char *text_content);
+void close_file_descriptor(int fd);
+char *allocate_buffer(char *file);
 int append_text_to_file(const char *filename, char *text_content);
 
 
