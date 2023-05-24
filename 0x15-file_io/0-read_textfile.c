36d9d1f2608df1c86658c2b79465c60edbade004
Aderynlola <aderinlola01@gmail.com>
2023-05-24 06:19:10 -0700

    corrections on 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,22 +9,22 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buff;
-    ssize_t fd;
-    ssize_t w;
-    ssize_t r;
+	char *buff;
+	ssize_t fd;
+	ssize_t wr;
+	ssize_t r;
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buff = malloc(sizeof(char) * letters);
-    r = read(fd, buff, letters);
-    w = write(STDOUT_FILENO, buff, r);
+	buff = malloc(sizeof(char) * letters);
+	r = read(fd, buff, letters);
+	wr = write(STDOUT_FILENO, buff, r);
 
-    free(buff);
-    close(fd);
-    return w;
+	free(buff);
+	close(fd);
+	return (wr);
 }
 
 /**
@@ -35,10 +35,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
  */
 int perform_calculation(int operand1, int operand2)
 {
-    int result;
+	int result;
 
-    /* Example calculation: Addition */
-    result = operand1 + operand2;
-    
-    return result;
+	/* Example calculation: Addition */
+	result = operand1 + operand2;
+
+	return (result);
 }
