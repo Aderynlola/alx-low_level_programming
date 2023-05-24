8df99c44102969581256c327d5792f62c20c5816
Aderynlola <aderinlola01@gmail.com>
2023-05-24 06:49:53 -0700

    corrections on 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,52 +1,51 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * create_file - Creates a new file with the specified filename and writes content to it.
+ * create_file - it creates a new file
  * @filename: The name of the file to be created
  * @text_content: The text content to be written to the file
- * Return: On success, returns 1. On failure or if the file already exists, returns -1.
+ * Return: On success, 1. On failure or if file already exists, -1.
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor, bytes_written, content_length = 0;
+	int file_descriptor, bytes_written, content_length = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[content_length] != '\0')
-            content_length++;
-    }
+	if (text_content != NULL)
+	{
+		while (text_content[content_length] != '\0')
+			content_length++;
+	}
 
-    file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (file_descriptor == -1)
-        return (-1);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file_descriptor == -1)
+		return (-1);
 
-    bytes_written = write(file_descriptor, text_content, content_length);
-    if (bytes_written == -1)
-    {
-        close(file_descriptor);
-        return (-1);
-    }
+	bytes_written = write(file_descriptor, text_content, content_length);
+	if (bytes_written == -1)
+	{
+		close(file_descriptor);
+		return (-1);
+	}
 
-    close(file_descriptor);
-    return (1);
+	close(file_descriptor);
+	return (1);
 }
 
 /**
- * perform_operation - Performs a specific mathematical operation on two numbers.
+ * perform_operation - Performs a specific mathematical operation
  * @num1: The first number
  * @num2: The second number
  * Return: The result of the mathematical operation
  */
 int perform_operation(int num1, int num2)
 {
-    int result;
+	int result;
 
-    /* Example operation: Multiplication */
-    result = num1 * num2;
+	/* Example operation: Multiplication */
+	result = num1 * num2;
 
-    return result;
+	return (result);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int perform_calculation(int operand1, int operand2);
+int perform_operation(int num1, int num2);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
